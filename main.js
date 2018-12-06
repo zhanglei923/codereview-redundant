5a067e9e40a99df4bcd1e738cea7c6f18632ba15
zhanglei <zhang.lei.923@gmail.com>
2018-12-07 06:11:16 +0800

    up

--- main.js
+++ main.js
@@ -28,6 +28,9 @@ filterFuns.push((fpath)=>{
     if(/\/lib\//g.test(fpath)) {
         //console.log(1, fpath);
         return false;
+    }else if(/\/oldcrm\//g.test(fpath)) {
+        //console.log(2, fpath);
+        return false;
     }else if(/\/i18n\//g.test(fpath)) {
         //console.log(2, fpath);
         return false;
