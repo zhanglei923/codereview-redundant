47d21def909077c959fdc83f806c26a63030a953
zhanglei <zhang.lei.923@gmail.com>
2018-12-07 04:36:02 +0800

    up

--- main.js
+++ main.js
@@ -26,25 +26,25 @@ codePath = pathutil.resolve(__dirname, codePath)
 let filterFuns = [];
 filterFuns.push((fpath)=>{
     if(/\/lib\//g.test(fpath)) {
-        console.log(1, fpath);
+        //console.log(1, fpath);
         return false;
     }else if(/\/i18n\//g.test(fpath)) {
-        console.log(2, fpath);
+        //console.log(2, fpath);
         return false;
     }else if(/\.bundle\./g.test(fpath)) {
-        console.log(3, fpath);
+        //console.log(3, fpath);
         return false;
     }else if(/\.min\./g.test(fpath)) {
-        console.log(4, fpath);
+        //console.log(4, fpath);
         return false;
     }else if(/\/default_cn/g.test(fpath)) {
-        console.log(5, fpath);
+        //console.log(5, fpath);
         return false;
     }else if(/\/province_data/g.test(fpath)) {
-        console.log(6, fpath);
+        //console.log(6, fpath);
         return false;
     }else if(/\-sdk\-/g.test(fpath)) {
-        console.log(8, fpath);
+        //console.log(8, fpath);
         return false;
     }    
     //if(/Ctrl\.js$/g.test(fpath)) {console.log(1, fpath);
