bd1a9af6231c75ef5e3b2091aa00208ba6454e4f
zhanglei <zhanglei@xiaoshouyi.com>
2018-12-07 15:00:37 +0800

    up

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ filterFuns.push((fpath)=>{
         return false;
     }else if(/\/oldcrm\//g.test(fpath)) {
         //console.log(2, fpath);
-        //return false;
+        return false;
     }else if(/\/i18n\//g.test(fpath)) {
         //console.log(2, fpath);
         return false;
@@ -50,7 +50,10 @@ filterFuns.push((fpath)=>{
     }else if(/\-sdk\-/g.test(fpath)) {
         //console.log(8, fpath);
         return false;
-    }    
+    }else if(/business\-widgets/g.test(fpath)) {
+        //console.log(8, fpath);
+        return false;
+    }      
     //if(/Ctrl\.js$/g.test(fpath)) {console.log(1, fpath);
     return true;
 })
