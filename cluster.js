31f85c9ced71340a09a295b9e37f306cac17eda9
zhanglei <zhang.lei.923@gmail.com>
2018-12-11 01:30:09 +0800

    up

---
+++ cluster.js
@@ -0,0 +1,18 @@
+const cluster = require('cluster');
+const http = require('http');
+const numCPUs = require('os').cpus().length;
+
+console.log(`numCPUs ${numCPUs}`);
+
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+  console.log(`Worker ${process.pid} started`);
+}
