666cb77a2f66da448dda6a19a91883b7fa836dd1
zhanglei@xiaoshouyi.com <zhang.lei.923@gmail.com>
2019-08-19 23:32:47 +0800

    up

--- codereview/runTasks.js
+++ codereview/runTasks.js
@@ -1,13 +1,7 @@
 var fs = require('fs');
 var _ = require('lodash');
 var md5Util = require("blueimp-md5")//https://github.com/blueimp/JavaScript-MD5
-var pathutil = require('path');
-var jsdiff = require('diff');
-let eachcontent = require('eachcontent-js')
-let scriptUtil = require('../util/scriptUtil')
 let multiTaskUtil = require('../util/multiTaskUtil')
-let lineBrkReg = /(\r\n){1,}/g
-let lineBrkString = '\r\n';
 
 let fpathCount = 0;
 let fpathMap = {};
