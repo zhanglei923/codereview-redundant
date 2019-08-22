bab52398797440439f8280577475f26d96d29372
zhanglei@xiaoshouyi.com <zhang.lei.923@gmail.com>
2019-08-23 00:17:03 +0800

    up

--- util/cacheUtil.js
+++ util/cacheUtil.js
@@ -16,8 +16,8 @@ let getL1L2_bk = (id)=>{
 }
 let getL1L2 = (id)=>{
     let l1 = id.substring(0,2);
-    let l2 = id.substring(2,4);
-    return `${l1}/${l2}`;
+    //let l2 = id.substring(2,4);
+    return `${l1}`;
 }
 let serRootFolder = (folder)=>{
     if(!fs.existsSync(folder)){
